3a03110a25b9c473e5f79290d157d69c4d4f8879
zabiyka8 <Dplehov88@gmail.com>
2024-12-22 17:15:43 +0300

    поправил добавление в корзину

--- src/main/java/com/example/topacademy_java311_plehov/DAO/implementations/OrderServiceImplementation.java
+++ src/main/java/com/example/topacademy_java311_plehov/DAO/implementations/OrderServiceImplementation.java
@@ -10,6 +10,7 @@ import com.example.topacademy_java311_plehov.model.shop.Order;
 import com.example.topacademy_java311_plehov.model.shop.OrderPosition;
 import com.example.topacademy_java311_plehov.model.shop.Profile;
 import com.example.topacademy_java311_plehov.repositories.OrderRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -30,109 +32,122 @@ public class OrderServiceImplementation implements OrderService {
 
     @Override
     public List<Order> findAll() {
-        return null;
+        return repo.findAll(); // Реализовать метод поиска всех заказов
     }
 
     @Override
     public Optional<Order> findById(int id) {
-        return Optional.empty();
+        return repo.findById((long) id);
     }
 
     @Override
     public Order save(Order order) {
-        return null;
+        return repo.save(order);
     }
 
     @Override
     public void deleteById(int id) {
+        repo.deleteById(Long.valueOf(id));
     }
-
 
     @Override
     public Optional<Order> findCartByUserId(Long profileId) {
         return repo.findById(profileId);
     }
 
+    @Transactional
     @Override
     public void addToCart(String email, int pizzaId) {
-
         ApplicationUser loggedUser = applicationUserService.loadUserByUsername(email);
-        // по id юзера ищем корзину (единственный неоплаченный заказ)
-        Order cart = findCartByUserId(loggedUser.getProfile().getId()).get();
-        // получаем 1 единицу товара со склада
-        Optional<Pizza> stockPositionToBuy = pizzaService.findById(pizzaId);
-        // на основе позиции со склада, которую собрался приобретать покупатель, формируем запись в чеке (корзине)
-        if (stockPositionToBuy.isPresent()) {
-            OrderPosition positionToAdd = OrderPosition.builder()
-                    .amount(1)
-                    .pizza(stockPositionToBuy.get())
-                    .order(cart)
-                    .build();
-            addToCart(cart, positionToAdd);
-            repo.save(cart);
+        Optional<Order> optionalCart = findCartByUserId(loggedUser.getProfile().getId());
+
+        if (optionalCart.isPresent()) {
+            Order cart = optionalCart.get();
+            Optional<Pizza> stockPositionToBuy = pizzaService.findById(pizzaId);
+
+            if (stockPositionToBuy.isPresent()) {
+                OrderPosition positionToAdd = OrderPosition.builder()
+                        .amount(1)
+                        .pizza(stockPositionToBuy.get())
+                        .order(cart)
+                        .build();
+                addToCart(cart, positionToAdd);
+                repo.save(cart);
+            } else {
+                throw new IllegalArgumentException("Такого товара нет в наличии");
+            }
         } else {
-            throw new IllegalArgumentException("Такого товара нет в наличии");
+            throw new IllegalStateException("Корзина не найдена для пользователя");
         }
     }
 
     @Override
     public Order findByOrderPositionId(int orderPositionId) {
-        return orderPositionService.findById(orderPositionId).get().getOrder();
+        return orderPositionService.findById(orderPositionId).orElseThrow(() ->
+                new IllegalArgumentException("Позиция заказа не найдена")).getOrder();
     }
 
     @Override
+    @Transactional
     public void pay(Order cart) {
-        cart.setStatus(Status.IS_PAID);
-        repo.save(cart);
-        removeFromStock(cart);
-        Profile currentProfile = cart.getProfile();
-        currentProfile.getOrders().add(
-                Order.builder()
-                        .status(Status.CART)
-                        .profile(currentProfile)
-                        .orderPositions(new HashSet<>())
-                        .build()
-        );
-        profileService.save(currentProfile);
+        if (cart != null) {
+            cart.setStatus(Status.IS_PAID);
+            repo.save(cart);
+            removeFromStock(cart);
+            Profile currentProfile = cart.getProfile();
+            currentProfile.getOrders().add(
+                    Order.builder()
+                            .status(Status.CART)
+                            .profile(currentProfile)
+                            .orderPositions(new HashSet<>())
+                            .build()
+            );
+            profileService.save(currentProfile);
+        } else {
+            throw new IllegalArgumentException("Корзина не может быть оплачена, так как не найдена");
+        }
     }
+
     private void removeFromStock(Order cart) {
         Set<OrderPosition> orderPositionSet = cart.getOrderPositions();
-        for (OrderPosition orderPosition : orderPositionSet) {
+        orderPositionSet.forEach(orderPosition -> {
             Set<Topping> toppings = orderPosition.getToppings();
             Set<TechCart> techCarts = orderPosition.getPizza().getTechCart();
-            for (TechCart techCart : techCarts) {
-                ingredientService.remove(techCart.getIngredient(), techCart.getIngredient().getAmount() * orderPosition.getAmount());
-            }
-            for (Topping topping : toppings) {
-                ingredientService.remove(topping.getIngredient(), topping.getIngredient().getAmount() * orderPosition.getAmount());
-            }
-        }
+            techCarts.forEach(techCart ->
+                    ingredientService.remove(techCart.getIngredient(), techCart.getIngredient().getAmount() * orderPosition.getAmount())
+            );
+            toppings.forEach(topping ->
+                    ingredientService.remove(topping.getIngredient(), topping.getIngredient().getAmount() * orderPosition.getAmount())
+            );
+        });
     }
 
     @Override
     public List<Order> ordersToDeliver() {
-        return null;
+        return repo.findAll().stream()
+                .filter(order -> order.getStatus().equals(Status.IS_PAID))
+                .collect(Collectors.toList());
     }
 
     @Override
     public void deliver(int orderId) {
+        // Реализовать логику для доставки заказа
     }
 
     private void addToCart(Order cart, OrderPosition positionToAdd) {
-
-        // Если товар присутствует в корзине, изменяем его количество, в противном случае, добавляем новую строку в корзину
         if (isStockPositionPresent(cart, positionToAdd)) {
             OrderPosition positionToIncrementAmount = cart.getOrderPositions().stream()
                     .filter(orderPosition -> orderPosition.getPizza().getId() == positionToAdd.getPizza().getId())
-                    .findFirst().get();
-                positionToIncrementAmount.setAmount(positionToIncrementAmount.getAmount() + 1);
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalArgumentException("Позиция для увеличения не найдена"));
+            positionToIncrementAmount.setAmount(positionToIncrementAmount.getAmount() + 1);
         } else {
-                // добавляем запись в чек (корзину)
-                cart.getOrderPositions().add(positionToAdd);
+            cart.getOrderPositions().add(positionToAdd);
         }
     }
+
     private boolean isStockPositionPresent(Order cart, OrderPosition positionToAdd) {
         return cart.getOrderPositions().stream()
-                .anyMatch(orderPosition -> orderPosition.getId() == positionToAdd.getId());
+                .anyMatch(orderPosition -> orderPosition.getPizza().getId() == positionToAdd.getPizza().getId());
     }
 }
